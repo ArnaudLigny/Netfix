c0519d1a9b6307372544dc735646ebc6c65502b1
Narno <arnaud@ligny.org>
2017-03-08 00:43:12 +0100

    Use DOMSubtreeModified to detect changes (hugely!)

--- extension/script.js
+++ extension/script.js
@@ -1,11 +1,10 @@
-function init() {
+function fix() {
   chrome.storage.sync.get({
     bannedSeries: '',
   }, function(items) {
     console.log('NETFIX GO!');
     var series = items.bannedSeries.split(/\r?\n/);
     $.each(series, function(key, value) {
-      console.log('- ' + value);
       $("[aria-label*='" + value + "']").parent('.slider-item').hide();
       $("[aria-label*='" + value + "']").parent().parent('.slider-item').hide();
     });
@@ -13,5 +12,7 @@ function init() {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  init();
+  $(document).bind('DOMSubtreeModified', function() {
+    fix();
+  });
 });
